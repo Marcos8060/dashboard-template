fd0eb62bdd46002be61642da9254863b854fe0be
MarcosDMT <marcosgav80@gmail.com>
2023-09-24 14:41:04 +0300

    added the sidebar header section

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <section className=''>
+      <header className='h-[10vh] shadow flex items-center justify-center text-xl font-bold'>Logo</header>
+    </section>
   )
 }
 
