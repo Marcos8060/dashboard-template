84787b1bed817bdd6c9e48326ea17b7fc77e72f7
MarcosDMT <marcosgav80@gmail.com>
2023-09-24 14:21:03 +0300

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <section className='bg-primary'>Header</section>
+  )
+}
+
+export default Header
---
+++ src/components/layout/index.js
@@ -0,0 +1,28 @@
+import React, { useState, ReactNode } from "react";
+import Header from "./Header";
+import Sidebar from "../sidebar";
+
+
+const Layout = ({ children }) => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <div
+        className={`${collapsed ? "w-16" : "w-72"} h-screen md:block hidden`}
+      >
+        <Sidebar
+          collapsed={collapsed}
+          setCollapsed={() => setCollapsed((prev) => !prev)}
+        />
+      </div>
+
+      <div className="flex-1 overflow-y-auto">
+        <Header collapsed={collapsed} />
+        <div className="bg-[#DFEAF0]">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
