2eaa3a59b30765de9a4d782642b42717918878dd
MarcosDMT <marcosgav80@gmail.com>
2023-10-01 16:06:56 +0300

    globalised theme

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,9 @@
 import "@/styles/globals.css";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "@/styles/theme";
 
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  return <>{getLayout(<Component {...pageProps} />)}</>;
+  return <ThemeProvider theme={theme}>{getLayout(<Component {...pageProps} />)}</ThemeProvider>;
 }
