457cb6bfca1a594588b2f6712c04db7d90a1a832
MarcosDMT <marcosgav80@gmail.com>
2023-10-01 15:54:14 +0300

    added account icon

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { FiChevronDown } from 'react-icons/fi'
 
 const Header = () => {
   return (
-    <section className='bg-primary shadow-2xl text-white p-4 h-[10vh]'>
-      <h1 className='text-xl font-semibold'>Header</h1>
+    <section className='bg-primary shadow-2xl text-white py-4 px-8 h-[10vh] flex items-center justify-between'>
+      <h1 className='text-xl'>Payouts</h1>
+      <div className='flex items-center gap-1'>
+        <img className='h-8 w-8 rounded-full object-cover' src="/images/doc.jpg" alt="" />
+        <FiChevronDown className='text-xl cursor-pointer' />
+      </div>
     </section>
   )
 }
